396d01b378039a080cf0845755000ad93faf9485
Marco Liang <marcoliang333@gmail.com>
2020-05-28 20:55:00 -0700

    Question 3

--- a1.py
+++ a1.py
@@ -15,7 +15,7 @@ def make_rand_8puzzle():
             break
     return puzzle
 
-def display(state):
+def display_8puzzle(state):
     for i in range(len(state)):
         if(state[i]==0):
             print('*', end=' ')
@@ -25,15 +25,15 @@ def display(state):
             print('\n', end='')
 
 #referencing code from search.py of aima-python code base
-def astar_search_modified(problem, h=None, display=False):
+def astar_search_modified(problem, h=None):
     start_time = time.time()
     h = memoize(h or problem.h, 'h')
-    result = best_first_graph_search_modified(problem, lambda n: n.path_cost + h(n), display)
+    result = best_first_graph_search_modified(problem, lambda n: n.path_cost + h(n))
     elapsed_time = time.time() - start_time
     print("elapsed time: {}s".format(elapsed_time))
     return result
 
-def best_first_graph_search_modified(problem, f, display=False):
+def best_first_graph_search_modified(problem, f):
     f = memoize(f, 'f')
     node = Node(problem.initial)
     frontier = PriorityQueue('min', f)
@@ -42,10 +42,8 @@ def best_first_graph_search_modified(problem, f, display=False):
     nodes_popped = 0
     while frontier:
         node = frontier.pop()
-        nodes_popped = nodes_popped+1
+        nodes_popped = nodes_popped + 1
         if problem.goal_test(node.state):
-            if display:
-                print(len(explored), "paths have been expanded and", len(frontier), "paths remain in the frontier")
             print("nodes popped: {}".format(nodes_popped))
             print("length of solution: {}".format(len(node.solution())))
             return node
@@ -102,7 +100,10 @@ class DuckPuzzle(Problem):
         blank = self.find_blank_square(state)
         new_state = list(state)
 
-        delta = {'UP': -3, 'DOWN': 3, 'LEFT': -1, 'RIGHT': 1}
+        if(blank > 1 and blank < 4):
+            delta = {'UP': -2, 'DOWN': 3, 'LEFT': -1, 'RIGHT': 1}
+        else:
+            delta = {'UP': -3, 'DOWN': 2, 'LEFT': -1, 'RIGHT': 1}
         neighbor = blank + delta[action]
         new_state[blank], new_state[neighbor] = new_state[neighbor], new_state[blank]
 
@@ -110,28 +111,80 @@ class DuckPuzzle(Problem):
 
     def goal_test(self, state):
         """ Given a state, return True if state is a goal state or False, otherwise """
-
         return state == self.goal
 
-    def check_solvability(self, state):
-        """ Checks if the given state is solvable """
+def make_rand_duck():
+    puzzle = DuckPuzzle((1, 2, 3, 4, 5, 6, 7, 8, 0)) # create duck puzzle as goal state
+    iterations = random.randrange(100,500) #iterations to run for
+    puzzle.state = puzzle.initial
+    for _ in range(0, iterations):
+        act = random.choice(puzzle.actions(puzzle.state))
+        puzzle.state = puzzle.result(puzzle.state, act)
+    puzzle.initial = puzzle.state
+    return puzzle
 
-        inversion = 0
-        for i in range(len(state)):
-            for j in range(i + 1, len(state)):
-                if (state[i] > state[j]) and state[i] != 0 and state[j] != 0:
-                    inversion += 1
+def display_duck(state):
+    for i in range(len(state)):
+        if(i==6):
+            print('  ', end='')
+        if(state[i]==0):
+            print('*', end=' ')
+        else:
+            print(state[i], end=' ')
+        if(i==1 or i==5 or i == 8):
+            print('\n', end='')
 
-        return inversion % 2 == 0
+def manhattan_duck(node):
+    goalRow = (2,0,0,1,1,1,1,2,2)
+    goalCol = (3,0,1,0,1,2,3,1,2)
+    return sum(abs(get_row(i) - goalRow[s]) + abs(get_col(i)- goalCol[s]) for i,s in enumerate(node.state))
 
+def max_of_both_duck(node):
+    return max(misplaced_tile(node), manhattan_duck(node))
 
-for i in range(1,11):       
-    print("Run {}".format(i))
-    s = make_rand_8puzzle()
-    display(s.state)
-    print("Using misplaced tile heuristic")
-    astar_search_modified(s, misplaced_tile, False)
-    print("Using Manhattan distance heuristic")
-    astar_search_modified(s, manhattan, False)
-    print("Using max of misplaced tile and Manhattan distance heuristics")
-    astar_search_modified(s, max_of_both, False)
+def get_row(index):
+    if index in [0,1]:
+        return 0
+    if index in [2,3,4,5]:
+        return 1
+    if index in [6,7,8]:
+        return 2
+
+def get_col(index):
+    if index in [0,2]:
+        return 0
+    if index in [1,3,6]:
+        return 1
+    if index in [4,7]:
+        return 2
+    if index in [5,8]:
+        return 3
+
+def Question2(iterations):
+    print("The 8 Puzzle")
+    for i in range(1,iterations+1):    
+        print("Run {}".format(i))
+        s = make_rand_8puzzle()
+        display_8puzzle(s.state)
+        print("Using misplaced tile heuristic")
+        astar_search_modified(s, misplaced_tile)
+        print("Using Manhattan distance heuristic")
+        astar_search_modified(s, manhattan)
+        print("Using max of misplaced tile and Manhattan distance heuristics")
+        astar_search_modified(s, max_of_both)
+
+def Question3(iterations):
+    print("The Duck Puzzle")
+    for i in range(1,iterations+1):    
+        print("Run {}".format(i))
+        s = make_rand_duck()
+        display_8puzzle(s.state)
+        print("Using misplaced tile heuristic")
+        astar_search_modified(s, misplaced_tile)
+        print("Using Manhattan distance heuristic")
+        astar_search_modified(s, manhattan_duck)
+        print("Using max of misplaced tile and Manhattan distance heuristics")
+        astar_search_modified(s, max_of_both_duck)
+
+Question2(10)
+Question3(10)
