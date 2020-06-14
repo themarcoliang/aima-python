a21e6317c733a591acc074b074d3c511816502dd
Marco Liang <marcoliang333@gmail.com>
2020-06-14 15:14:56 -0700

    Q3

--- a2_q1.py
+++ a2_q1.py
@@ -5,7 +5,7 @@ def rand_graph(p,n):
     for i in range(0,n):
         friendship[i] = list(())
     for first in range(0,n-1):
-        for second in range(0,n-1):
+        for second in range(first,n):
             if(second in friendship[first] or first == second):
                 continue
             else:
@@ -16,4 +16,5 @@ def rand_graph(p,n):
     
     return friendship
             
-# rand_graph(0.2,5)
+# graph=rand_graph(0.2,5)
+# print(graph)
---
+++ a2_q3.py
@@ -0,0 +1,60 @@
+from a2_q1 import rand_graph
+from a2_q2 import check_teams
+import csp
+import time
+
+# This is a class that adds a variable to track unassignments of a CSP problem
+# References InstrucCSP(CSP) in csp.ipynb notebook
+class unassignCSP(csp.CSP):
+    def __init__(self,variables, domains, neighbors, constraints):
+        super().__init__(variables,domains,neighbors,constraints)
+        self.unassignments = 0
+    
+    def unassign(self, var, assignment):
+        super().unassign(var,assignment)
+        self.unassignments += 1
+
+def edge_count(graph):
+    edge = 0
+    for i in graph:
+        edge += sum(j > i for j in graph[i])
+    return edge
+
+def run_q3():
+    graphs = [rand_graph(0.1,31), rand_graph(0.2,31), rand_graph(0.3,31),
+    rand_graph(0.4, 31), rand_graph(0.5,31), rand_graph(0.6,31)]
+    count = 0
+    for g in graphs:
+        numPeople = 31
+        elapsedTime = 0
+        assigns = 0
+        unassigns = 0
+        teamLimit = 1
+        count += 1
+        while(True):
+            domain = csp.UniversalDict([i for i in range(1, teamLimit+1)])
+            tempCSP = csp.CSP(list(g.keys()), domain, g, csp.different_values_constraint)
+            problem = unassignCSP(tempCSP.variables, tempCSP.domains, tempCSP.neighbors, tempCSP.constraints)
+            csp.AC3(problem,removals=[])
+            startTime = time.time()
+            result = csp.backtracking_search(problem, order_domain_values=csp.lcv, select_unassigned_variable=csp.mrv, inference=csp.forward_checking)
+            elapsedTime += time.time() - startTime
+            assigns += problem.nassigns
+            unassigns += problem.unassignments
+            teamLimit += 1
+            # print("Increasing teamsize to", teamLimit)
+            if(result != None and check_teams(g,result)): #correct result is found
+                break
+        
+        print("=================================================")
+        print("Run", count)
+        print("Solved with Backtracking and AC3 and Forward Checking Inference")
+        print("Probability:", count * 0.1)
+        print("Number of People:", numPeople)
+        print("Number of Edges:", edge_count(g))
+        print("Number of Teams:", len(set(result.values())))
+        print("Elapsed Time(in seconds):", elapsedTime)
+        print("Total Number of Assignments:", assigns)
+        print("Total Number of Unassignments", unassigns)         
+
+run_q3()
