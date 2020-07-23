cd975568ea8a2cbfe36be355b8d6993427499407
Marco Liang <marcoliang333@gmail.com>
2020-07-23 16:14:28 -0700

    load file

--- a4.py
+++ a4.py
@@ -1,7 +1,26 @@
-def check_format(rule):
-    return False
+# is_atom and is_letter are taken from assignment specifications
+def is_atom(s):
+    if not isinstance(s, str):
+        return False
+    if s == "":
+        return False
+    return is_letter(s[0]) and all(is_letter(c) or c.isdigit() for c in s[1:])
 
-def load(parameters):
+def is_letter(s):
+    return len(s) == 1 and s.lower() in "_abcdefghijklmnopqrstuvwxyz"
+
+def check_format(split_rule):
+    if is_atom(split_rule[0]) == False:
+        return False
+    if split_rule[1] != "<--":
+        return False
+    for item in split_rule[2:]:
+        # print("checking item", item)
+        if item != "&" and not is_atom(item):
+            return False
+    return True
+
+def load(heads, parameters):
     if len(parameters) == 0:
         print("Error: missing file name")
         return
@@ -15,9 +34,22 @@ def load(parameters):
         return
     lines = f.readlines()
     for line in lines:
-        if check_format(line) == False:
+        split_line = line.split()
+        if check_format(split_line) == False:
             print("Error: {} is not a valid knowledge base".format(parameters[0]))
+            heads = {}
             return
+        else:
+            head = split_line[0]
+            items_to_append = []
+            del split_line[0]
+            del split_line[0]
+            for item in split_line:
+                if item == "&":
+                    continue
+                else:
+                    items_to_append.append(item)
+            heads[head] = items_to_append
 
 def tell(parameters):
     print("tell")
@@ -27,6 +59,7 @@ def infer_all():
     print("infer_all")
 
 def driver():
+    loaded = False
     while(True):
         command = input("kb> ")
         if command == "":
@@ -35,10 +68,21 @@ def driver():
         function = split_command[0]
         del split_command[0]
         if(function == "load"):
-            load(split_command)
+            loaded = True
+            #reset heads and true_atoms since it's a new KB whenever load is called
+            heads = {}
+            true_atoms = {}
+            load(heads, split_command)
+            print(heads)
         elif(function == "tell"):
+            if loaded == False:
+                print("Error: No KB Loaded")
+                continue
             tell(split_command)
         elif(function == "infer_all"):
+            if loaded == False:
+                print("Error: No KB Loaded")
+                continue
             infer_all()
         else:
             print("Error: unknown command \"{}\"".format(function))
